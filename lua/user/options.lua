61d1c919f192245906b9051317acc4b51539fe9a
Omar Ayman <se.omarayman@gmail.com>
2023-03-18 19:32:16 +0200

    edited foldcolumn to 0

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -44,7 +44,7 @@ vim.opt.relativenumber = true
 vim.opt.guifont = { "JetBrainsMono Nerd Font Mono", ":h10" }
 vim.opt.incsearch = true
 
-vim.opt.foldcolumn = '1' -- '0' is not bad
+vim.opt.foldcolumn = '0' -- '0' is not bad
 vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
